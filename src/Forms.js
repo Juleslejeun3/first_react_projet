9c129f6fdd93452666f0ebf7e422ffdd5c479099
Camara Mamadou <juleslejeun3@gmail.com>
2022-03-23 16:24:16 +0000

    react_projet

--- src/Forms.js
+++ src/Forms.js
@@ -1,28 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-function App() {
-  return (
-  <div>
-  <header class="list_header">
-  <ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-</ul>
-  </header>
-  <div class='backgr'>
-  <form class="formular">
+function Form() {
+  render() {
+    return (
+      <div>
+      <form>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Email</label>
@@ -68,10 +48,12 @@ function App() {
   </div>
   <button type="submit" class="btn btn-primary">Sign in</button>
 </form>
-</div>
-  </div>
-    
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
+ ReactDOM.render(
+  <Form/>,
+  document.getElementById('formular')
+);
